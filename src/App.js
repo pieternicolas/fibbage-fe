3f234ddda80cf97bf842614faf7b51eca1e0700b
Nicolas <nicolaspieter@yahoo.com>
2019-02-23 09:24:20 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import io from 'socket.io-client'
+
+import logo from './logo.svg'
+import './App.css'
+
+const socket = io.connect('http://localhost:4200')
 
 class App extends Component {
+  componentDidMount() {
+    socket.on('news', (data) => {
+      console.log(data);
+      socket.emit('my other event', { my: 'data' });
+    })
+  }
+
+  handleClick = () => {
+    socket.emit('my other event', { test: 'adsfadfs' })
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +26,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <button onClick={() => this.handleClick()}>adsfadsfas</button>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -21,8 +37,8 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
